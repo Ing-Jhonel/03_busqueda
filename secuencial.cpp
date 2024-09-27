d1086407fa22a5a09c6cd1b31e1af487cef81a94
Jhonel Apaza <jgapazap@unjbg.edu.pe>
2024-09-27 15:12:26 -0500

    Busqueda Secuencial Desordenada terminada

--- secuencial.cpp
+++ secuencial.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 
+int secuencialDes(double a[], int, double);
+void imprimir(double a[], int);
+
 int main(){
 	
 	int tamanio;
@@ -29,20 +32,44 @@ int main(){
 		system("cls");
 		if(opcion=='a' || opcion=='b'){
 			cout << "Arreglo: ";
+			imprimir(arr, tamanio);
 			cout << endl << "Elemento a buscar = " << valor << endl;
 		}
 		switch(opcion){
 			case 'a':
+				cout << "Posicion del elemento: ";
+				cout << secuencialDes(arr, tamanio, valor);
 				break;
 			case 'b':
 				break;
 			case 'c':
+				cout << "Saliendo..." << endl;
 				break;
 			default:
+				cout << "Numero invalido";
 				break;
 		}
 		cout << endl << endl;
 		system("pause");
 	} while(opcion!='c');
 	return 0;
+}
+
+int secuencialDes(double a[], int n, double dato){
+	int pos=-1;
+	int i=0;
+	while(i<n && a[i]!=dato){
+		i++;
+	}
+	if(i<n){
+		pos=i;
+	}
+	return pos;
+}
+
+void imprimir(double a[], int n){
+	
+	for(int i=0; i<n; i++){
+		cout << a[i] << " ";
+	}
 }
